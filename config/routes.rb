adefcfd49f589c6f68815f67c61fc5b840e01178
joaquimgmess <joaquimgmess21@gmail.com>
2025-05-16 23:17:21 -0300

    Add product subscription feature with email notifications and rich text support for product descriptions

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
   root "products#index"
 end
